8198516a6b797cfc408296bfc698dabe44373352
neagix <neagix@users.noreply.github.com>
2017-08-06 05:06:30 +0200

    Fixed polling in EXI driver Other miscellaneous compilation fixes

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -2029,9 +2029,8 @@ EXPORT_SYMBOL_GPL(gpiod_count);
  */
 int gpio_direction_is_output(unsigned gpio)
 {
-	struct gpio_desc *desc = &gpio_desc[gpio];
-
-	if (!gpio_is_valid(gpio))
+	struct gpio_desc *desc = gpio_to_desc(gpio);
+	if (desc == NULL)
 		return -EINVAL;
 
 	return test_bit(FLAG_IS_OUT, &desc->flags);
