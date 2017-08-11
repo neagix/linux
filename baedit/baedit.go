5008e66a3aeab8d08de8dc6572fbd65922e1b3b8
neagix <neagix@users.noreply.github.com>
2017-08-11 20:07:00 +0200

    Allow querying current bootargs

--- baedit/baedit.go
+++ baedit/baedit.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-    "io/ioutil"
-    "os"
-    "fmt"
-    "bytes"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 var (
 	startMarker = []byte(`mark.start=1 `)
-	endMarker = []byte(` mark.end=1`)
+	endMarker   = []byte(` mark.end=1`)
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Fprintf(os.Stderr, "Usage: baedit zImage 'new kernel command line'\n")
+	if len(os.Args) != 3 && len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: baedit zImage ['new kernel command line']\n")
 		os.Exit(1)
 	}
 	zImage := os.Args[1]
-	newBootArgs := os.Args[2]
 
-    b, err := ioutil.ReadFile(zImage)
-    if err != nil {
+	b, err := ioutil.ReadFile(zImage)
+	if err != nil {
 		panic(err)
 	}
-	
+
 	fmt.Printf(">OK: %d bytes read\n", len(b))
-	
+
 	start := bytes.Index(b, startMarker)
 	if start == -1 {
 		panic("cannot find start marker")
@@ -36,29 +35,33 @@ func main() {
 	if availBytes == -1 {
 		panic("cannot find end marker")
 	}
-	
-	bootArgs := string(b[start:start+availBytes])
+
+	bootArgs := string(b[start : start+availBytes])
 	fmt.Printf("current  bootargs = '%s'\n", bootArgs)
-	
+
+	if len(os.Args) < 3 {
+		return
+	}
+
+	newBootArgs := os.Args[2]
 	l := len(newBootArgs)
 	if l > availBytes {
 		panic("new command line is too long")
 	}
-	
+
 	// pad with spaces
-	for i:=0;i<availBytes-l;i++ {
+	for i := 0; i < availBytes-l; i++ {
 		newBootArgs += " "
 	}
 	fmt.Printf("replaced bootargs = '%s'\n", newBootArgs)
 
-	for i:=0;i<availBytes;i++ {
+	for i := 0; i < availBytes; i++ {
 		b[start+i] = newBootArgs[i]
 	}
-	
+
 	err = ioutil.WriteFile(zImage, b, 0775)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf(">OK: %d bytes written\n", len(b))
 }
-
